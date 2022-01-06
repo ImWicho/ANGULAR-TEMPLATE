e6bf13d24f2c328537476528e29dd6bf20ce1ab3
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2022-01-06 13:28:33 -0600

    [ADD] sub

--- src/app/main/pages/main/main.component.ts
+++ src/app/main/pages/main/main.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { Store } from '@ngrx/store';
 import { AppState } from '@redux/main.reducer';
 import { SidenavService } from '@services/sidenav.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit  {
+export class MainComponent implements OnInit, OnDestroy  {
   @ViewChild('matDrawer') drawer!: MatDrawer;
   isResponsive = false;
-
+  sub! : Subscription;
   constructor(private store: Store<AppState>,
               private sidenavService: SidenavService) { }
 
   ngOnInit(): void {
-    this.store.select('app').subscribe((data) => this.isResponsive = data.isResponsive);
+    this.sub = this.store.select('app').subscribe((data) => this.isResponsive = data.isResponsive);
     this.sidenavService.toggle$.subscribe(() => this.drawer.toggle());
+  }
+
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
   }
 
 }
