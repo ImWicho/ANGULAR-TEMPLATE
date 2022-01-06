e5830573b5c6d818744f1e0d20d7ba2b50a115f7
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2022-01-05 19:05:55 -0600

    [ADD] main routes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent, children: [
+    { path: 'example', loadChildren: () => import('./pages/example/example.module').then((m) => m.ExampleModule) }
+   ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './pages/main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
